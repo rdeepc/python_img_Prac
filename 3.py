4a368763b4f675b05e67716f8b9ef02843679a69
rdeepc <dpr0413@gmail.com>
2017-01-29 21:06:11 +0600

    12

--- 3.py
+++ 3.py
@@ -1,8 +1,16 @@
-from scipy.misc import imresize
-import matplotlib.pyplot as plt
 import numpy as np
-img = plt.imread('t2.jpg')
-data = np.concatenate([img_i[np.newaxis] for img_i in img], axis=0)
-print(data)
-plt.imshow(data)
+import matplotlib.pyplot as plt
+row_y=2
+col_x=3
+img = np.zeros([row_y,col_x], dtype=np.uint8)
+img[0][0]=255
+img[0][1]=242
+img[0][2]=0
+img[1][0]=255
+img[1][1]=0
+img[1][2]=0
+
+
+print(img)
+plt.imshow(img)
 plt.show()
