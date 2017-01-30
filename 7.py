534e6ff6371bd4b1a924166f14f050a4d03be49a
rdeepc <dpr0413@gmail.com>
2017-01-30 17:26:44 +0600

    15

---
+++ 7.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+import os.path as path
+import glob2
+image_list = []
+imgExt = ("png","jpg","jpeg")
+for filename in glob2.glob('ds/*/**.*'): #assuming gif
+    if((path.splitext(filename)[1][1:]) in imgExt):
+        img=plt.imread(filename)
+        image_list.append(img)
+
+print(len(image_list))
+
